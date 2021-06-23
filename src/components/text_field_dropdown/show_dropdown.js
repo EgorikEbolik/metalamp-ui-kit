5a7882bbdcbbba5fe2474b374da75a8443183248
Egor <egor2005efor@gmail.com>
2021-06-24 01:05:28 +0700

    Сделал вёрстку полей ввода и дропдауна. Также для дропдауна добавил функционал JS, чтобы можно было менять значения полей

---
+++ src/components/text_field_dropdown/show_dropdown.js
@@ -0,0 +1,38 @@
+area = document.querySelector(".dropdown-area__text");
+list = document.querySelector(".dropdown-area__list");
+items = list.querySelectorAll(".dropdown-area__list__item")
+
+
+area.onclick = () => {
+  list.classList.toggle("d-none");
+}
+
+function changeValue() {
+  for (let i = 0; i < items.length; i++) {
+    const element = items[i];
+    const elementPlusButton = element.querySelector(".dropdown-area__list__item__counter__plus-button");
+    const elementMinusButton = element.querySelector(".dropdown-area__list__item__counter__minus-button");
+    let elementString = element.querySelector(".dropdown-area__list__item__counter__value");
+    let elementValue = parseInt(elementString.textContent.match(/\d+/));
+
+    elementPlusButton.onclick = () => {
+      elementValue += 1;
+      elementString.innerHTML = String(elementValue);
+    };
+
+    elementMinusButton.onclick = () => {
+      if(elementValue >= 1) {
+        elementValue -= 1;
+        elementString.innerHTML = String(elementValue);
+      }
+      
+    };
+
+    
+
+
+    
+  }
+}
+
+changeValue()
